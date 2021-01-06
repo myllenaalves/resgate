8cad29000bf0f0170a9f42c42c264b321c0cd18b
myllenaalves <mal4@cin.ufpe.br>
2021-01-06 17:15:46 -0300

    Structuring omniauth-google authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+
   has_many :pets
   has_many :posts
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/auth/google_oauth2', as: 'google_oauth2_login'
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
 end
