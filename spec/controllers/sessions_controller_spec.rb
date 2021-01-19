bdb20a6c7be67951488dbb20024a841b64a62e58
myllenaalves <mal4@cin.ufpe.br>
2021-01-19 19:42:04 -0300

    Updating specs to sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  it "authenticate a User" do
+    get :create, :params => {"state"=>"acc687a3136c1f8185ff0bc3d4a4ced6841b67e760fcbf4a", "code"=>"4/0AY0e-g68aig7L52i6ErepXuOyBcK8Z8ChqAVpuoBMoz-4JB-TDp-Hz2NuvWebYdMM3fbzw", "scope"=>"email profile https://www.googleapis.com/auth/contacts openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile", "authuser"=>"0", "prompt"=>"consent"}
 
+    expect(response).to have_http_status(:ok)
+  end
 end
